79bcb755ede3f2b4731e57bc68575b37faeaacf4
YukiOtake <djotkyu@gmail.com>
2018-05-22 13:26:48 +0900

    fastCommit

---
+++ Assets/Script/AirTaps.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class AirTaps : MonoBehaviour, IFocusable, IInputClickHandler
+{
+    public Material blue, green,red;
+
+    public void OnFocusEnter()//gazeに入ったら
+    {
+        GetComponent<Renderer>().material = blue;//blueをアタッチ
+    }
+
+    public void OnFocusExit()//gazeから出たら
+    {
+        GetComponent<Renderer>().material = green;//アタッチ
+    }
+
+    public void OnInputClicked(InputClickedEventData eventData)//エアタップされたら
+    {
+        //GetComponent<Renderer>().material = red;//色を変更
+        Color rand = new Color(Random.value, Random.value, Random.value, 1.0f);//ランダムで色を生成
+        GetComponent<Renderer>().material.color = rand;//アタッチ
+        transform.Rotate(new Vector3(0, 15, 0));
+    }
+}
---
+++ Assets/Script/UnityChanController.cs
@@ -0,0 +1,37 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanController : MonoBehaviour, IInputClickHandler
+{
+    private Animator animator;//Animatorを入れる変数
+    private AnimatorStateInfo currentState, previousState;
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();//UnityちゃんのAnimatorにアクセスする
+        currentState = animator.GetCurrentAnimatorStateInfo(0);
+        previousState = currentState;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // "Next"フラグがtrueの時の処理
+        if (animator.GetBool("Next"))
+        {
+            // 現在のステートをチェックし、ステート名が違っていたらブーリアンをfalseに戻す
+            currentState = animator.GetCurrentAnimatorStateInfo(0);
+            if (previousState.nameHash != currentState.nameHash)
+            {
+                animator.SetBool("Next", false);
+                previousState = currentState;
+            }
+        }
+    }
+    public void OnInputClicked(InputClickedEventData eventData)//タップされたら
+    {
+        Debug.Log("in!!");
+        animator.SetBool("Next", true);//アニメーション
+
+    }
+}
